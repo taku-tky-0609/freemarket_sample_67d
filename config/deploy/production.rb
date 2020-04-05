abf2303404d25cf3ab9ff2dffa9012b368115a1a
ta1ka2shi3 <60285334+ta1ka2shi3@users.noreply.github.com>
2020-04-05 21:58:32 +0900

    z

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
+server '52.196.216.95', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
