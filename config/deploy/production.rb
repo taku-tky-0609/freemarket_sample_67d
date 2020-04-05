2a8adf680d794b7b59cff83fde57d4c3d411a0df
ta1ka2shi3 <60285334+ta1ka2shi3@users.noreply.github.com>
2020-04-05 22:13:27 +0900

    modify production rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,3 @@
-server '52.196.216.95', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
@@ -8,6 +7,7 @@ server '52.196.216.95', user: 'ec2-user', roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
+server '52.196.216.95', user: 'ec2-user', roles: %w{app db web}
 
 
 # role-based syntax
