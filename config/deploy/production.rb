c542b70e1ac14e0f5723c8f372d3ddd2899d155b
ta1ka2shi3 <60285334+ta1ka2shi3@users.noreply.github.com>
2020-04-05 22:14:46 +0900

    debug

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-server '52.196.216.95', user: 'ec2-user', roles: %w{app db web}
+serverrrrrrrr '52.196.216.95', user: 'ec2-user', roles: %w{app db web}
 
 
 # role-based syntax
