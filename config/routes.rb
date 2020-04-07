590d01616987e6c04e707f567b0d1bac8cb19088
irietoshinari <60064033+irietoshinari@users.noreply.github.com>
2020-04-07 23:22:56 +0900

    必要そうなcontrollerを作った

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'addresses/show'
+  get 'brands/show'
+  get 'credit_cards/index'
+  get 'categories/index'
+  get 'items_comments/create'
   devise_for :users
   root 'items#index'
 end
