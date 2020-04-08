4b4237f2138baebcf96e45abb28ff364068a9fee
irietoshinari <60064033+irietoshinari@users.noreply.github.com>
2020-04-08 21:44:20 +0900

    deviseモデルの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
-  has_many :items
-  has_many :comments
-  has_many :likes
-  has_many :items_statuses
-  has_one :credit_card
-  has_many :addresses
-  accepts_nested_attributes_for :addresses
+         :recoverable, :rememberable,  :validatable
+   validates :name, presence: true, uniqueness: true
+   has_many :items
+   has_many :comments
+   has_many :likes
+   has_many :items_statuses
+   has_one :credit_card
+   has_many :addresses
+   accepts_nested_attributes_for :addresses
 
 end
