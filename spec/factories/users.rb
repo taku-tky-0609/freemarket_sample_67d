a87698823bc5fd88435bb752d0dd46b44217be0c
keisuke-oyatu <58317630+keisuke-oyatu@users.noreply.github.com>
2020-04-10 20:03:30 +0900

    user faker の作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,21 +1,21 @@
 FactoryBot.define do
 
   factory :user do
-    nickname               {"otani_keita"}
-    email                  {"keita.otani@gmail.com"}
-    last_name              {"圭太"}
-    first_name             {"大谷"}
-    last_name_kana         {"けいた"}
-    first_name_kana        {"おおたに"}
-    address                {"東京都"}
-    phone_number           {"09012345678"}
-    password               {"otani1234"}
-    reset_password_token   {}
-    reset_password_sent_at {}
-    remember_created_at    {}
-    self_introduction      {}
-    sales                  {}
-    point                  {}
-    icon                   {}
+    nickname               {Faker::Name.name}
+    email                  {Faker::Internet.email}
+    last_name              {Faker::Name.last_name}
+    first_name             {Faker::Name.first_name}
+    last_name_kana         {Faker::Name.last_name}
+    first_name_kana        {Faker::Name.first_name}
+    address                {Faker::Internet.email}
+    phone_number           {Faker::PhoneNumber.phone_number}
+    password               {password}
+    reset_password_token   {password}
+    reset_password_sent_at {password}
+    remember_created_at    {Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)}
+    self_introduction      {Faker::Lorem.word}
+    sales                  {Faker::Number.number}
+    point                  {Faker::Number.number}
+    icon                   {Faker::Avatar.image}
   end
 end
