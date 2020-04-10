97be6ca60fd12b58196caf212fdde4521269e9d8
taku-tky-0609 <60353242+taku-tky-0609@users.noreply.github.com>
2020-04-11 00:58:32 +0900

    Merge branch 'master' of https://github.com/takeru1201/freemarket_sample_67d

---
+++ spec/factories/users.rb
@@ -0,0 +1,21 @@
+FactoryBot.define do
+
+  factory :user do
+    nickname               {Faker::Name.name}
+    email                  {Faker::Internet.email}
+    last_name              {Faker::Name.last_name}
+    first_name             {Faker::Name.first_name}
+    last_name_kana         {Faker::Name.last_name}
+    first_name_kana        {Faker::Name.first_name}
+    address                {Faker::Internet.email}
+    phone_number           {Faker::PhoneNumber.phone_number}
+    password               {password}
+    reset_password_token   {password}
+    reset_password_sent_at {password}
+    remember_created_at    {Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)}
+    self_introduction      {Faker::Lorem.word}
+    sales                  {Faker::Number.number}
+    point                  {Faker::Number.number}
+    icon                   {Faker::Avatar.image}
+  end
+end
