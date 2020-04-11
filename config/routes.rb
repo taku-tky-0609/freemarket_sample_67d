0457685116d42deb857a4ae3facea21dc156fb19
irietoshinari <60064033+irietoshinari@users.noreply.github.com>
2020-04-11 12:55:29 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
+  root to: 'home#index'
+  
+  
+
   # root to: "home#index"
   # get 'addresses/show'
   # get 'brands/show'
