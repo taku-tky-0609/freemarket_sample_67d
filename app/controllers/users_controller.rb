b9dd6cca1d14cb0622e11c80cee2ac88c120a630
taku-tky-0609 <60353242+taku-tky-0609@users.noreply.github.com>
2020-04-13 00:12:15 +0900

    Merge branch 'master' of https://github.com/takeru1201/freemarket_sample_67d

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,26 @@
 class UsersController < ApplicationController
   def index
   end
+  
+  def new
+    @user = User.new
+    @user.address.build
+  end
+
+  def create
+    @user = User.new(user_params)
+    binding.pry
+    @user.save
+    redirect_to users_path(@user)
+  end
 
   def show
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :password, :password_confirmation, :last_name, :first_name, :last_name_kana, :first_name_kana, :phone_number, address_attributes:[:postal_code, :prefecture, :municipality, :house_number])
+  end
+
 end
