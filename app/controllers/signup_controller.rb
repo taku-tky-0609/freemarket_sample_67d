a75ae4ab83951d3902b40614a627de1b2a1b8a6d
irietoshinari <60064033+irietoshinari@users.noreply.github.com>
2020-04-14 00:22:46 +0900

    ウィザードの実装

---
+++ app/controllers/signup_controller.rb
@@ -0,0 +1,78 @@
+class SignupController < ApplicationController
+
+  def step1
+    @user = User.new # 新規インスタンス作成
+    
+  end
+
+  def step2
+    # step1で入力された値をsessionに保存
+    session[:nickname] = user_params[:nickname]
+    session[:email] = user_params[:email]
+    session[:password] = user_params[:password]
+    session[:password_confirmation] = user_params[:password_confirmation]
+    @user = User.new # 新規インスタンス作成
+  end
+
+  def step3
+    # step2で入力された値をsessionに保存
+    session[:last_name] = user_params[:last_name]
+    session[:first_name] = user_params[:first_name]
+    session[:last_name_kana] = user_params[:last_name_kana]
+    session[:first_name_kana] = user_params[:first_name_kana]
+    @user = User.new # 新規インスタンス作成
+  end
+
+  def step4
+    session[:address] = user_params[:address]
+    session[:phone_number] = user_params[:phone_number]
+    @user = User.new
+  end
+
+
+  def create
+    @user = User.new(
+      nickname: session[:nickname], # sessionに保存された値をインスタンスに渡す
+      email: session[:email],
+      password: session[:password],
+      password_confirmation: session[:password_confirmation],
+      last_name: session[:last_name], 
+      first_name: session[:first_name], 
+      last_name_kana: session[:last_name_kana], 
+      first_name_kana: session[:first_name_kana], 
+      address: session[:address],
+      phone_number: session[:phone_number],
+    )
+    if @user.save
+      session[:id] = @user.id
+      redirect_to done_signup_index_path
+    else
+      render '/signup/registration'
+    end
+
+  end
+
+  def done
+    sign_in User.find(session[:id]) unless user_signed_in?
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(
+      :nickname, 
+      :email, 
+      :password, 
+      :password_confirmation, 
+      :last_name, 
+      :first_name, 
+      :last_name_kana, 
+      :first_name_kana, 
+      :address,
+      :phone_number,
+
+  )
+  end
+
+end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,16 @@ Rails.application.routes.draw do
   root 'items#index'
   root to: 'home#index'
   resources :users, only: :show
-  
+
+  resources :signup do
+    collection do
+      get 'step1'
+      post 'step2'
+      get 'step3'
+      get 'step4'
+      get 'done' # 登録完了後のページ
+    end
+  end
   # get 'addresses/show'
   # get 'brands/show'
   # get 'credit_cards/index'
