80a639f447232c0803ddabbabdb7a904f6cffbe8
takeru1201 <ldn.asktmhmh@icloud.com>
2020-04-16 23:13:56 +0900

    追加:usersテーブルにbirthdayカラム
    a

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_15_152659) do
+ActiveRecord::Schema.define(version: 2020_04_16_141034) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -128,6 +128,7 @@ ActiveRecord::Schema.define(version: 2020_04_15_152659) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "address_id"
+    t.date "birthday"
     t.index ["address_id"], name: "index_users_on_address_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
