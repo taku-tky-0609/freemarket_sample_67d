3f38bf29c92e7fe5b39b4d3c391aa37ac425848b
taku-tky-0609 <60353242+taku-tky-0609@users.noreply.github.com>
2020-04-19 02:10:54 +0900

    正規表現１

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_20_112629) do
+
+ActiveRecord::Schema.define(version: 2020_04_15_152659) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -21,7 +22,6 @@ ActiveRecord::Schema.define(version: 2020_04_20_112629) do
     t.string "house_number", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "house_number", null: false
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
