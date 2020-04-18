7c20d4a59d97b23f6b64a151759d2b72136ca701
taku-tky-0609 <60353242+taku-tky-0609@users.noreply.github.com>
2020-04-18 22:11:15 +0900

    validation 正規表現1

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,4 @@
 class Address < ApplicationRecord
   belongs_to :user
+  validates :postal_code, presence: true, length: { maximum: 8 }, format: { with: /\A\d{3}-\d{4}\z/i, message: 'のフォーマットが不適切です。記述例 000-0000'}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,13 @@ class User < ApplicationRecord
   has_one :credit_card
   has_one :address
   accepts_nested_attributes_for :address
+
+  validates :last_name, presence: true, length: { maximum: 35 }, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '日本語でお願いします please use Japanese characters'}
+  validates :first_name, presence: true, length: { maximum: 35 }, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: '日本語でお願いします please use Japanese characters'}
+  validates :last_name_kana, presence: true, length: { maximum: 35 }, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい'}
+  validates :first_name_kana, presence: true, length: { maximum: 35 }, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい'}
+  # validates :password, presence: true, length: { in: 7..128 }, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d!@#\$%\^\&*\)\(+=._-]{7,128}\z/i,  message: 'は英字と数字両方を含むパスワードを設定してください'}
+  # validates :password_confirmation, presence: true, length: { in: 7..128 }, format: { with:  /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d!@#\$%\^\&*\)\(+=._-]{7,128}\z/i, message: 'は英字と数字両方を含むパスワードを設定してください'}
+  validates :phone_number, presence: true, format: { with: /\A[0-9]+\z/, message: 'ハイフン（-）入っちゃってる？いらないよ'}
+  
 end
