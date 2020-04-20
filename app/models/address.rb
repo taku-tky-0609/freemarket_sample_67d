57328771b81c41c9ebd5c7ee23021de63c807b37
taku-tky-0609 <60353242+taku-tky-0609@users.noreply.github.com>
2020-04-20 21:34:15 +0900

    バリデーション 正規表現1

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,5 @@
 class Address < ApplicationRecord
   belongs_to :user, optional: true
-  validates :prefectures, :postal_code, presence: true
+  validates :prefectures, presence: true
+  # validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/, message: 'のフォーマットが不適切です。記述例 000-0000'}
 end
