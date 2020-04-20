20284f706c713c12d0f6d666b5924cece34e1a88
takeru1201 <ldn.asktmhmh@icloud.com>
2020-04-20 20:34:06 +0900

    修正:addressesのpostal.code

---
+++ db/migrate/20200420112629_change_postal_code_to_adresses.rb
@@ -0,0 +1,9 @@
+class ChangePostalCodeToAdresses < ActiveRecord::Migration[5.2]
+  def up
+    change_column :addresses, :postal_code, :string
+  end
+
+  def down
+    change_column :addresses, :postal_code, :integer
+  end
+end
