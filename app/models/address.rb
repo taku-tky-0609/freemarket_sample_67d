e99fc71e13e7396225b50a3c9109e161688eb250
taku-tky-0609 <60353242+taku-tky-0609@users.noreply.github.com>
2020-04-20 22:51:55 +0900

    バリデーション 正規表現2

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,7 @@
 class Address < ApplicationRecord
   belongs_to :user, optional: true
   validates :prefectures, presence: true
-  # validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/, message: 'のフォーマットが不適切です。記述例 000-0000'}
+  validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/, message: 'のフォーマットが不適切です。記述例 000-0000'}
+  validates :municipality, presence: true
+  validates :house_number, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_04_15_152659) do
+ActiveRecord::Schema.define(version: 2020_04_20_112629) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
