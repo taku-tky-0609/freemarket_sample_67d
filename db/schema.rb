538275a3af1dbda8989be2252d7f83a23fd57730
takeru1201 <ldn.asktmhmh@icloud.com>
2020-04-22 23:53:07 +0900

    追加:eunm

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2020_04_21_134130) do
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "postal_code", null: false
-    t.string "prefectures", null: false
+    t.integer "prefectures", default: 0, null: false
     t.string "municipality", null: false
     t.string "building"
     t.string "house_number", null: false
@@ -27,14 +27,6 @@ ActiveRecord::Schema.define(version: 2020_04_21_134130) do
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "item", null: false
     t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "customer_id", null: false
-    t.string "card_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -52,14 +44,6 @@ ActiveRecord::Schema.define(version: 2020_04_21_134130) do
     t.integer "user_id", null: false
     t.string "customer_id", null: false
     t.string "card_id", null: false
-  end
-
-  create_table "credits", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "customer_id", null: false
-    t.string "card_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "evaluations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
