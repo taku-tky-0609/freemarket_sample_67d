082e7891fdc98136c4d6ad07245ea6b7e05d7b23
takeru1201 <ldn.asktmhmh@icloud.com>
2020-05-03 20:53:51 +0900

    削除:item_images
    active_storageの実装に先駆けitem_imagesテーブル(画像)を削除
    
    マイグレファイルを新しく作成しました

--- db/migrate/20200503114737_drop_item_images.rb
+++ db/migrate/20200503114737_drop_item_images.rb
@@ -1,11 +1,13 @@
-class CreateItemImages < ActiveRecord::Migration[5.2]
-  def change
+class DropItemImages < ActiveRecord::Migration[5.2]
+  def up
+    drop_table :item_images
+  end
+
+  def down
     create_table :item_images do |t|
       t.string :image, null: false
       t.integer :item, null: false
       t.timestamps
-    end
+    end  
   end
 end
-
-
