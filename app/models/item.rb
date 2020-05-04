ab7d4ae3c67ee71e8a21874be1b5c471568bb118
taku-tky-0609 <60353242+taku-tky-0609@users.noreply.github.com>
2020-05-04 20:34:00 +0900

    active-storageの実装2

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,6 @@ class Item < ApplicationRecord
   # has_many :likes
   # has_many :items_statuses
   # has_many :item_images
-<
   has_many_attached :images
   # belongs_to :brand
   belongs_to :user
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_05_03_173810) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -34,7 +33,6 @@ ActiveRecord::Schema.define(version: 2020_05_03_173810) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "postal_code", null: false
@@ -55,7 +53,7 @@ ActiveRecord::Schema.define(version: 2020_05_03_173810) do
   end
 
   create_table "categoris", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "category_name", null: false
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "ancestry"
@@ -148,6 +146,5 @@ ActiveRecord::Schema.define(version: 2020_05_03_173810) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "item_images", "items"
   add_foreign_key "users", "addresses"
 end
