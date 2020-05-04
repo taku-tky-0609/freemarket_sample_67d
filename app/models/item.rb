836d3693c2e735d28f512d7abea710306db805f4
takeru1201 <ldn.asktmhmh@icloud.com>
2020-05-04 20:03:31 +0900

    削除:item_imagesモデル

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
   # belongs_to :brand
   belongs_to :user
   belongs_to :categori
-  accepts_nested_attributes_for :item_images
+  # accepts_nested_attributes_for :item_images
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :delivery_fee
