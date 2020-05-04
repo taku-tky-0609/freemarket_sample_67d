c7d7d9c019590f6aad4f3bba6213d1cc381b639f
ta1ka2shi3 <60285334+ta1ka2shi3@users.noreply.github.com>
2020-05-04 22:18:24 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -53,7 +53,7 @@ ActiveRecord::Schema.define(version: 2020_05_03_173810) do
   end
 
   create_table "categoris", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "category_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "ancestry"
@@ -138,7 +138,5 @@ ActiveRecord::Schema.define(version: 2020_05_03_173810) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-
-  add_foreign_key "item_images", "items"
   add_foreign_key "users", "addresses"
 end
