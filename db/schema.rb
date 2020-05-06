2a99f67546c1779980a4c0a28d23203727ae59c4
takeru1201 <ldn.asktmhmh@icloud.com>
2020-05-06 22:50:52 +0900

    更新:maigure

--- db/schema.rb
+++ db/schema.rb
@@ -36,12 +36,12 @@ ActiveRecord::Schema.define(version: 2020_05_03_173810) do
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "postal_code", null: false
-    t.string "prefectures", null: false
     t.string "municipality", null: false
     t.string "building"
     t.string "house_number", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "prefectures"
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
@@ -53,7 +53,7 @@ ActiveRecord::Schema.define(version: 2020_05_03_173810) do
   end
 
   create_table "categoris", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "category_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "ancestry"
@@ -146,7 +146,5 @@ ActiveRecord::Schema.define(version: 2020_05_03_173810) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-
-  add_foreign_key "item_images", "items"
   add_foreign_key "users", "addresses"
 end
