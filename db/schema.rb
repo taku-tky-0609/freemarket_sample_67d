bca347c0c94466ed9259ad18cec15db53a56be1a
takeru1201 <ldn.asktmhmh@icloud.com>
2020-05-07 21:59:57 +0900

    修正:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -12,15 +12,36 @@
 
 ActiveRecord::Schema.define(version: 2020_05_05_134954) do
 
+  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.string "postal_code", null: false
+    t.string "prefectures", null: false
     t.string "municipality", null: false
     t.string "building"
     t.string "house_number", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "prefectures"
     t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
@@ -30,16 +51,8 @@ ActiveRecord::Schema.define(version: 2020_05_05_134954) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "customer_id", null: false
-    t.string "card_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "category_name", null: false
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "ancestry"
@@ -51,14 +64,6 @@ ActiveRecord::Schema.define(version: 2020_05_05_134954) do
     t.integer "user_id", null: false
     t.string "customer_id", null: false
     t.string "card_id", null: false
-  end
-
-  create_table "credits", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "customer_id", null: false
-    t.string "card_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "evaluations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -142,8 +147,6 @@ ActiveRecord::Schema.define(version: 2020_05_05_134954) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-
-  add_foreign_key "item_images", "items"
   add_foreign_key "items", "brands"
   add_foreign_key "users", "addresses"
 end
