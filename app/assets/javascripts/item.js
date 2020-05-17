ae03dc3598d22a27a3d85739849d951bd4659afa
taku-tky-0609 <60353242+taku-tky-0609@users.noreply.github.com>
2020-05-17 22:02:29 +0900

    active-sotrage-jQuery lightBox2

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -9,15 +9,15 @@ $(function () {
     var reader = new FileReader();
     
     reader.onload = function(e){
-      $(target).closest('.preview_area').prepend(`
-        <div class="select_image" data-id=${id-1} data-lightbox="abc">
-          <a href="${e.target.result}" data-lightbox="group">
-            <img src=${e.target.result} id="image_preview" width="300">
-          </a>
-            <p><a href="#">編集</a></p>
-            <p><a class="remove_image">削除</a></p>
-        </div>
-      `);
+      $(target).closest('.prepend_area').prepend(`
+      <div class="select_image" data-id=${id-1} data-lightbox="abc">
+        <a href="${e.target.result}" data-lightbox="group">
+          <img src=${e.target.result} id="image_preview" width="300">
+        </a>
+        <p><a href="#">編集</a></p>
+        <p><a class="remove_image">削除</a></p>
+    </div>
+  `);
       var imagesLength = $('.select_image').length;
       
       $(target).closest('.pre-content').css('width', `calc(100% - ${20 * (imagesLength % 5)}%)`);
@@ -32,7 +32,6 @@ $(function () {
     
     var filesArray = $('.item_images_hidden').val().split(',');
     var imagesLength = $('.select_image').length;
-    var defualtImageText =  $('.pre-content').children('p').html('<p>クリックをしてアップロード</p>');
     // $(document).on('DOMSubtreeModified propertychange', function(){
 
     //   if (imagesLength + 1 === 4) {
@@ -121,119 +120,3 @@ $(function () {
     
   });
 });
-
-
-
-
-
-
-
-
-
-// $(function(){
-//   $("form").submit(function(e){
-        
-//     if ($(".pre-content").html() == ""){
-//         alert("画像を選択してください");
-      
-//         return false;
-       
-        
-//   //   if ($('input[type="hidden"').attr("value") == deleteSrc){
-//   //   $(this).remove();
-//   // }
-//       };
-      
-//       var inputHidden = []
-//       inputHidden.push($('input[type="hidden"]'));
-//       console.log(inputHidden[1])
-//   })
-
-//   $(".hidden-field").on('change', function(e){
-//     var files = e.target.files;
-
-//     //画像ファイル以外の場合は何もしない
-//     // if(file.type.indexOf("image") < 0){
-//     //   return false;
-//     // }
-//     // if ($(".pre-content").html() == ""){
-//     //   alert("画像を選択してください");
-//     // }
-    
-//     // var d = (new $.Deferred()).resolve();
-//     $.each(files,function(i,file){
-      
-//       //画像ファイル以外の場合は何もしない
-      
-//       if(file.type.indexOf("image") < 0){
-//         alert('画像を選択してください');
-//           return false;
-//         }
-//       return previewImage(file);
-//     });
-//   })
-//   var previewImage = function(imageFile){
-   
-//     var reader = new FileReader();
-//     // var img = new Image();
-    
-//     reader.onload = function(e){
-
-//       //画像のhtml要素を作成
-//       var preview = `<li class="item-pic">
-                      
-//                         <img class="item-pic__image" src= ${e.target.result}>
-                      
-//                       <div class="item-pic__button">
-//                         <a class="item-pic__button-edit" href="#" style= "color:#333333;" >編集</a>
-//                         <a class="item-pic__button-delete"style= "color:#333333; cursor: pointer" >削除</a>
-//                       </div>
-//                     </li>`;
-      
-      
-//       // `<li class= "item-pic"><img class="item-pic__image" src = ${e.target.result} ></li>`
-
-//      //画像をフォームの隣に出力
-//      $('.pre-content').prepend(preview);
-
-//     //フォームのリサイズ
-//       $(".label-content").resize('width', "calc(800px - ($('.item-pic').length * 20%" );
-     
-//       // 画像が一つ以上ある場合ボックスの文字を写真マークに変更
-//     if ($(".item-pic").length > 1){
-      
-//       //画像が一枚出力されたら、文字をカメラマークに変更
-//       $(".text1").replaceWith("<i class='fas fa-camera'></i>");
-//     }
-//     if ($(".item-pic").length > 4){
-//       //画像が五枚の場合はフォームを非表示
-//       $(".label-content").hide();
-//     }
-
-//     $(document).on('click', '.item-pic__button-delete', function(){
-//     //  var hiddenInput = $('input[type="hidden"]').attr("type", "file");
-     
-//        var deleteSrc = ($(this).closest('li').children().attr('src'));
-//        $(this).closest('li').remove();
-//       //　file_fieldフォームを削除
-//       // $('input[type="file"]').remove();
-//       });
-//     };
-//     reader.readAsDataURL(imageFile);
-//   };
-// });
-
-   // cloned = $(".label-content").clone(ture);
-      
-      //新しいフォームを追加
-      // $(".post__drop__box__container2").show();
-    // $('.pre-content2').prepend(preview);
-    // ($(".item-pic:gt(6)").prepend)
-    //   $('.pre-content2 ').prepend(preview);
-      
-    // }
-    //   console.log($(".item-pic").length);
-        // var inputHidden = $('input[type="hidden"').length
-    // console.log(inputHidden)
-    // if ($('input[type="hidden"').attr("value") == deleteSrc){
-    //   ;
