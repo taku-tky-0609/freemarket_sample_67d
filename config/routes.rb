5d7f17353684075dcd84520d116cc77849caa0dd
taku-tky-0609 <60353242+taku-tky-0609@users.noreply.github.com>
2020-05-19 00:49:55 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
     get 'get_category_children', defaults: { format: 'json' }
     get 'get_category_grandchildren', defaults: { format: 'json' }
     get 'myList', to: 'items#myList'
-   
+  end
   end
 
   resources :items do
