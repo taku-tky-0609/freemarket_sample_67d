43bd1df78836d281f3cdd4aa73eae34b56a704ec
keisuke-oyatu <58317630+keisuke-oyatu@users.noreply.github.com>
2020-05-23 18:15:51 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,14 +5,7 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
-    # @item.item_images.build
     @item.build_brand
-    # @item.brand.build
-    # @category_parent_array = ["---"]
-    # # @category_parent_array = Category.where(ancestry: nil)
-    # Category.where(ancestry: nil).each do |parent|
-    #   @category_parent_array << parent.name
-
       # def get_category_children
       #   @category_children = Category.find(params[:parent_id]).children
       # end
