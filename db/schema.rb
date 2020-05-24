534ef439d5840928889137618a4c0445b8f9366c
ta1ka2shi3 <kosugetakashi@icloud.com>
2020-05-24 23:43:30 +0900

    birthday update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_08_122803) do
+ActiveRecord::Schema.define(version: 2020_05_24_064503) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -123,7 +123,7 @@ ActiveRecord::Schema.define(version: 2020_05_08_122803) do
     t.integer "sales"
     t.integer "point"
     t.text "icon"
-    t.date "birthday", null: false
+    t.date "birthday"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
