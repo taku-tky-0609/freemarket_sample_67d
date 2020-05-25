1a277a7afad21c10213a3f6a30d6e26f3d551636
keisuke-oyatu <58317630+keisuke-oyatu@users.noreply.github.com>
2020-05-25 21:11:01 +0900

    category機能の実装

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,10 @@ class CategoriesController < ApplicationController
   def index
     @parents = Category.all
   end
+
+  def show
+    @category = Category.find(params[:id])
+    @items = @category.items
+  end
 end
+
