c2b648745ef2478c9a14f5d8d8d20b9e9214db6a
taku-tky-0609 <60353242+taku-tky-0609@users.noreply.github.com>
2020-05-26 00:41:35 +0900

    carrierwave previewの実装

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -1,31 +1,42 @@
 $(function (){
   function buildFileField(index) {
-    html = `<div class="new_item_page_container_main_label">
+    html = `<div class="new_item_page_container_main_label" data-index="${index}">
               <input class= "js-file" type= "file"name="item[item_images_attributes][${index}][src]" id="item_item_images_attributes_${index}_src">
               <div class="js-remove">削除</div>
             </div>`;  
     return html;
   };
+  const buildImg = (index, url)=> {
+    const html = `<img data-index="${index}" src="${url}" width="20%" height="160px">`;
+    return html;
+  };
 // file_fieldのnameに動的なindexをつける為の配列
 let fileIndex = [1,2,3,4,5,6,7,8,9,10];
+lastIndex = $('.new_item_page_container_main_label').data('index');
+fileIndex.splice(0, lastIndex);
 
-  $("body").on('change', '.js-file', function(){
+  $("body").on('change', '.js-file', function(e){
+    const targetIndex = $(this).parent().data('index')
+    const file = e.target.files[0];
+    const blobUrl = window.URL.createObjectURL(file);
+    $('.prepend_area').prepend(buildImg(targetIndex, blobUrl));
+
+
+
     $(".prepend_area").append(buildFileField(fileIndex[0]));
     fileIndex.shift();
     // 末尾の数に1足した数を追加する
     fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
-    return false;
+    
 
   })
 
   $(document).on("click", '.js-remove', function(){
+    const targetIndex = $(this).parent().data('index');
     $(this).parent().remove();
-
+    if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
+    $(`img[data-index="${targetIndex}"]`).remove();
   });
-
-
-
-
 })
 
 
