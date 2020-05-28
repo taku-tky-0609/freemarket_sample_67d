08fc3ee22a97a7ffb33a81ac3dd191dfe94091fb
keisuke-oyatu <58317630+keisuke-oyatu@users.noreply.github.com>
2020-05-28 19:59:21 +0900

    address model test 完了

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -15,10 +15,20 @@ describe ItemsController, type: :controller do
   end
   describe 'POST #create' do 
     it "商品登録に成功した場合root_pathに遷移すること" do
+      # expect{
+      #   item_params = FactoryBot.attributes_for(:item)
+      #   post :create, params: { item: item_params }
+      # }.to change{Item.count}.by(1)
+      # expect(response).to render_template(root_path)
+      
       expect{
-        item_params = FactoryBot.attributes_for(:item)
+        # user = User.create(user:user)
+        item = Item.create(item_params)
+        item_image = ItemImage.create(id: "1", src:"111", item_id:"1")
+        # item_params = FactoryBot.attributes_for(:item)
+        binding.pry
         post :create, params: { item: item_params }
-      }.to change(Item, :count).by(1)
+      }.to change{Item.count}.by(1)
       expect(response).to render_template(root_path)
     end
   end
