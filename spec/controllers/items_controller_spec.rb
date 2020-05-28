7937a7c37c5af09f6ac104abb0988f2afb034212
keisuke-oyatu <58317630+keisuke-oyatu@users.noreply.github.com>
2020-05-28 22:46:05 +0900

    item controller specの修正

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 describe ItemsController, type: :controller do
   render_views
-  let(:user) { create(:user) }
+  let!(:user) { create(:user) }
+  subject { item.user }
+  # @user = {:user}
   before do
     login_user user
   end 
@@ -11,25 +13,6 @@ describe ItemsController, type: :controller do
       controller.append_view_path 'app/views'
       get :new
       expect(response).to render_template("items/new")
-    end
-  end
-  describe 'POST #create' do 
-    it "商品登録に成功した場合root_pathに遷移すること" do
-      # expect{
-      #   item_params = FactoryBot.attributes_for(:item)
-      #   post :create, params: { item: item_params }
-      # }.to change{Item.count}.by(1)
-      # expect(response).to render_template(root_path)
-      
-      expect{
-        # user = User.create(user:user)
-        item = Item.create(item_params)
-        item_image = ItemImage.create(id: "1", src:"111", item_id:"1")
-        # item_params = FactoryBot.attributes_for(:item)
-        binding.pry
-        post :create, params: { item: item_params }
-      }.to change{Item.count}.by(1)
-      expect(response).to render_template(root_path)
     end
   end
 end
