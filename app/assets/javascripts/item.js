6ee9feffa475d10ec8724db2cc60f2d23d885ca2
taku-tky-0609 <60353242+taku-tky-0609@users.noreply.github.com>
2020-05-31 02:22:13 +0900

    item-editting-商品編集2

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -1,7 +1,7 @@
 $(function (){
   function buildFileField(index) {
     html = `<div class="new_item_page_container_main_label" data-index="${index}">
-              <input id = "js-file" class= "js-file" type= "file"name="item[item_images_attributes][${index}][src]" id="item_item_images_attributes_${index}_src">
+              <input id = "js-file" class= "js-file" type= "file"name="item[item_images_attributes][${index}][src]" data-index="${index}" id="item_item_images_attributes_${index}_src">
             </div>`;  
     return html;
   };
@@ -11,26 +11,49 @@ $(function (){
       <a href="${url}" data-lightbox="group">
         <img data-index="${index}" src="${url}" data-lightbox="group">
       </a>
-      <i class="fas fa-trash-alt"></i>
+      <div class="editting-box">
+        <i class="fas fa-image"></i>
+        <i class="fas fa-trash-alt"></i>
+      </div>
     </div>`;
     return html;
   };
 
+if ($(".image-box").length > 0){
+  $(".pre-content").css('width', `calc(100% - ${20 * ($(".image-box").length  % 5)}%)`)
+  if ($(".image-box").length === 4) {
+    $('.text').html('<i class="fas fa-camera"></i>');
+  } else if ($(".image-box").length === 5) {
+    $('.pre-content').css('display', 'none');
+  }
+}
 // file_fieldのnameに動的なindexをつける為の配列
 let fileIndex = [1,2,3,4,5,6,7,8,9,10];
 lastIndex = $('.new_item_page_container_main_label:last').data('index');
-console.log(lastIndex)
 fileIndex.splice(0, lastIndex);
+$('.hidden-destroy').hide();
+// $('#saved-js-file').hide();
+
+$(document).on("click", '.fa-image', function(){
+  const targetIndex = $(this).parent().parent().data('index');
+  
+  $(`input[data-index="${targetIndex}"].js-file`).click();
+});
 
   $(document).on('change', '.js-file', function(e){
-    const targetIndex = $(this).parent().data('index')
+    const targetIndex = $(this).parent().data('index');
     const file = e.target.files[0];
     const blobUrl = window.URL.createObjectURL(file);
     if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
+      
       img.setAttribute('src', blobUrl);
-    } else{
-    $('.prepend_area').prepend(buildImg(targetIndex, blobUrl));
-    }
+    } 
+    else{
+      $('.prepend_area').prepend(buildImg(targetIndex, blobUrl));
+      $(".form-file").prepend(buildFileField(fileIndex[0]));
+      fileIndex.shift();
+      fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
+    };
     var imagesLength = $('.image-box').length;
     $('.pre-content').css('width', `calc(100% - ${20 * (imagesLength % 5)}%)`);
     var imagesLength = $('.image-box').length;
@@ -38,15 +61,17 @@ fileIndex.splice(0, lastIndex);
       $('.text').html('<i class="fas fa-camera"></i>');
     } else if (imagesLength === 5){
       $('.pre-content').css('display', 'none');
+      
     };
 
-    $(".form-file").prepend(buildFileField(fileIndex[0]));
-    fileIndex.shift();
-    fileIndex.push(fileIndex[fileIndex.length - 1] + 1)
+
   })
 
+
   $(document).on("click", '.fa-trash-alt', function(){
-    const targetIndex = $(this).parent().data('index');
+    const targetIndex = $(this).parent().parent().data('index');
+    const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
+    if (hiddenCheck) hiddenCheck.prop('checked', true);
     $(`.new_item_page_container_main_label[data-index="${targetIndex}"]`).remove();
     var imagesLength = $('.image-box').length;
     $('.pre-content').css('width', `calc(100% - ${20 * (imagesLength % 5 - 1)}%)`);
@@ -57,7 +82,7 @@ fileIndex.splice(0, lastIndex);
       $('.text').html('<p class="text">クリックをしてアップロード</i>');
     };
     if ($('.js-file').length == 0) { $('.pre-content').append(buildFileField(fileIndex[0]));}
-    $(`div[data-index="${targetIndex}"]`).remove();
+    $(`.image-box[data-index="${targetIndex}"]`).remove();
   });
 
 })
