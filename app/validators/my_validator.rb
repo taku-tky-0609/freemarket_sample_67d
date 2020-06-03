7a6baf36b9a0bc331892adb3d1154d95a6eacb76
keisuke-oyatu <58317630+keisuke-oyatu@users.noreply.github.com>
2020-06-03 22:26:18 +0900

    カテゴリーエラーハンドリングの実装

--- app/validators/my_validator.rb
+++ app/validators/my_validator.rb
@@ -1,7 +1,7 @@
 class MyValidator < ActiveModel::Validator
 
   def validate(record)
-    if record.category_id == 0
+    if record.category_id == 0 or 1
       record.errors.add('category_id', 'は最後まで選択してください')
     end
   end
