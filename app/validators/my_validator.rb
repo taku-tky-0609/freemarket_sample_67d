7e7ee82cb9eeac2a1d6dbea2225a0ce75beb2ce1
irietoshinari <60064033+irietoshinari@users.noreply.github.com>
2020-06-05 22:55:15 +0900

    商品購入完了画面のviewを修正

--- app/validators/my_validator.rb
+++ app/validators/my_validator.rb
@@ -1,7 +1,7 @@
 class MyValidator < ActiveModel::Validator
 
   def validate(record)
-    if record.category_id == 0 or 1
+    if record.category_id == 0 
       record.errors.add('category_id', 'は最後まで選択してください')
     end
   end
